a00dd50e5e4179715971e9a14499dccca9efa064
Carlos Mestre <carlos.mestre@fit-tecnologia.org.br>
2016-10-05 16:12:48 -0300

    handling form submition and disable submit button

--- angular-forms/app/hero-form.component.ts
+++ angular-forms/app/hero-form.component.ts
@@ -13,9 +13,16 @@ export class HeroFormComponent {
     powers = ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
     model = new Hero(18, 'Dr. IQ', this.powers[0], 'Chuck Overstreet');
     submitted = false;
+    active = true;
 
     onSubmit() {
         this.submitted = true;
+    }
+
+    newHero() {
+        this.model = new Hero(42, '', '');
+        this.active = false;
+        setTimeout(() => this.active = true, 0);
     }
 
     // TODO: Remove this when we're done
